e48a0d713ffc9aeb5a22c9928e30545051686c0e
Static9star <officialydhruv979@gmail.com>
2024-10-06 14:32:32 +0530

    Router chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Fetch from './component/Fetch';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Welcome from './Welcome';
 import UserDetails from './UserDetails';
 import NotFound from './NotFound';
@@ -10,12 +10,12 @@ const App = () => {
   return (
     <>
         <Router>
-          <Switch>
+          <Routes>
             <Route path='/' element={<Welcome />} />
             <Route path='users' element={<Fetch />} />
             <Route path="/user/:id" element={<UserDetails />} />
             <Route path='*' element={<NotFound />} />
-          </Switch>
+          </Routes>
         </Router>
         
         
